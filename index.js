cddf405d1e53dfaea109501e74d89819f3e06008
Kartik Jolapara <42518907+codingmickey@users.noreply.github.com>
2023-04-14 12:19:38 +0530

    fix: update the seminar code

--- index.js
+++ index.js
@@ -1,80 +1,59 @@
-// Description: This is the main file of the API
-
-// Importing the express module
 const express = require('express');
 const app = express();
-
-// Array of finances
-let finances = [
-  {
-    // Name of the transaction
-    name: 'Gift for mummy',
-    // Bill, EMI, Entertainment, Food, Drinks, Fuel, Groceries, Health, Investment, Shopping, Transfer, Travel
-    category: 'Food',
-    // Date of the transaction
-    date: '2021-05-01',
-    // Amount of the transaction
-    amount: 1000,
-    // Unique ID of the transaction
-    id: 1
-  },
-  {
-    name: 'Pav Bhaji with Friends',
-    category: 'Food',
-    date: '2021-05-01',
-    amount: 1000,
-    id: 2
-  }
-];
 
 // Ignore (for now, ask me later if you want to know)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Root route
+const expenses = [
+  {
+    title: 'Pav Bhaji',
+    amount: 200,
+    date: '14-04-23',
+    category: 'Food',
+    id: 1
+  }
+];
+app.get('/expenses', (req, res) => {
+  res.send(expenses);
+});
+
+app.post('/expenses', (req, res) => {
+  const amount = req.body.amount;
+  const title = req.body.title;
+  const date = req.body.date;
+  const expense = {
+    amount: amount,
+    title: title,
+    date: date,
+    id: 2
+  };
+  expenses.push(expense);
+  res.send('expense added!');
+});
+
 app.get('/', (req, res) => {
-  res.send('API is working!! Lessgo :D');
+  res.send('API is working!');
 });
 
-// Get all finances
-app.get('/finances', (req, res) => {
-  res.send(finances);
-});
-
-// Get a single finance
-app.get('/finances/:id', (req, res) => {
-  let id = req.params.id;
-  let finance = finances.find((finance) => finance.id == id);
-  res.send(finance);
-});
-
-// Add a finance
-app.post('/finances', (req, res) => {
-  let finance = req.body;
-  finances.push(finance);
-  res.send('Added');
-});
-
-// Update a finance
-app.put('/finances/:id', (req, res) => {
-  let id = req.params.id;
-  let finance = finances.find((finance) => finance.id == id);
-  finance.name = req.body.name;
-  finance.category = req.body.category;
-  finance.date = req.body.date;
-  finance.amount = req.body.amount;
+app.put('/expenses', (req, res) => {
+  let id = req.body.id;
+  let expense = expenses.find((expense) => expense.id == id);
+  expense.name = req.body.name;
+  expense.category = req.body.category;
+  expense.date = req.body.date;
+  expense.amount = req.body.amount;
   res.send('Updated');
 });
 
-// Delete a finance
-app.delete('/finances/:id', (req, res) => {
-  let id = req.params.id;
-  let finance = finances.find((finance) => finance.id == id);
-  let index = finances.indexOf(finance);
-  finances.splice(index, 1);
+// Delete a expense
+app.delete('/expenses', (req, res) => {
+  let id = req.body.id;
+  let expense = expenses.find((expense) => expense.id == id);
+  let index = expenses.indexOf(expense);
+  expenses.splice(index, 1);
   res.send('Deleted');
 });
-
-app.listen(5000, () => {
-  console.log('Server Listening on port 5000');
+app.listen(3001, () => {
+  console.log('Server is running!!');
 });
